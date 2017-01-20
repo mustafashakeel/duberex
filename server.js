cac8fc40379067bfcb30378951b22734f2b07d00
Mustafa Qureshi <mustafashakeel@gmail.com>
2017-01-19 23:32:22 -0800

    fixing cors issue on heroku

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.headers["Access-Control-Allow-Origin", "*"];
+	  res.header["Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"];	
   if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
